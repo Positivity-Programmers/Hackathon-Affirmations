c45be6dda14a10d14d963bb3b3befcb47bad4962
Joha Wilson <johawilsonatu@icloud.com>
2024-03-23 20:07:53 +0000

    added styles to generator header and button

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,24 @@ import { affirmations } from "./affirmations.js";
 
 const affirmBtn = document.getElementById('affirm-btn')
 const displayAffirm = document.getElementById('display-affirm')
+let arrayList = []
 
 function generateRandomAffirm() {
     const randomAffirmation = affirmations[Math.floor(Math.random() * affirmations.length)]
-    displayAffirm.innerHTML = `<h1> ${randomAffirmation}</h1>`;
+    displayAffirm.innerHTML = `<p class="text-center p-5"> ${randomAffirmation}</p>`;
+    
+    /**for (let i = 0; i < affirmations.length; i++) {
+        if (randomAffirmation === affirmations[i]) {
+            affirmations.splice(i, 1);
+          setTimeout(() => {
+              
+          }, 2000);
+          console.log(true);
+        } else {
+            displayAffirm.innerHTML = `<p class="text-center p-5"> ${randomAffirmation}</p>`;
+        }
+    }
+    **/
     
 }
 affirmBtn.addEventListener('click', generateRandomAffirm)
-
