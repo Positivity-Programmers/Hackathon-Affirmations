952f0b00a63e670df41be50170e7a1d7f61c8000
Jaycode88 <126462009+Jaycode88@users.noreply.github.com>
2024-03-24 18:33:00 +0000

    Add back translate with btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Affirmations generator script
 import { affirmations } from "./affirmations.js";
 
 
@@ -12,3 +13,21 @@ function generateRandomAffirm() {
 }
 affirmBtn.addEventListener('click', generateRandomAffirm)
 
+// Google Translate Script
+document.getElementById('loadTranslate').onclick = function() {
+    var translateScript = document.createElement('script');
+    translateScript.type = 'text/javascript';
+    translateScript.src = '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit';
+    document.body.appendChild(translateScript);
+
+    // Initialize translation widget once the script is loaded
+    window.googleTranslateElementInit = function() {
+      new google.translate.TranslateElement({
+        pageLanguage: 'en',
+        layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+      }, 'google_translate_element');
+    };
+
+    // Hide the button after it's clicked
+    this.style.display = 'none';
+  };
